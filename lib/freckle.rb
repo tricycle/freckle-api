aa1944d60dba82a0b73d52a0b50a9341b0bb9a98
Matthew Fallshaw <matthew.fallshaw@gmail.com>
2010-07-02 17:01:09 +1000

    prefer project.name over project.project[:name] (likewise for entry, preserve legacy behaviour)

--- lib/freckle.rb
+++ lib/freckle.rb
@@ -1,15 +1,26 @@
 require 'yajl' unless defined?(Yajl)
 require 'streamly' unless defined?(Streamly)
 
+LEGACY_KEYS = %w[project entry]  # backward compatibility
+
 module Apis
   module Freckle
     module ApiObject
       def initialize(hash)
-        @hash = hash
+        legacy_key = hash.keys.select {|k| LEGACY_KEYS.include?(k) }.first
+        if legacy_key
+          @hash = hash[legacy_key]
+        else
+          @hash = hash
+        end
       end
       
       def method_missing(method, *args)
-        @hash[method.to_s]
+        unless LEGACY_KEYS.include?(method)
+          @hash[method.to_s]
+        else
+          @hash
+        end
       end
     end
   end
